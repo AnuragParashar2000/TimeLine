c7e9704f06dc58438cf426f6a6591434b25dd72f
Anurag Parashar <anuragparashar@Anurags-MacBook-Air.local>
2026-01-29 18:14:33 +0530

    Refactor: Move Firebase config to .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAi8QqZgYhc72frxJTycrPjQs24usbRIhQ",
-    authDomain: "timeline-5d7cf.firebaseapp.com",
-    projectId: "timeline-5d7cf",
-    storageBucket: "timeline-5d7cf.firebasestorage.app",
-    messagingSenderId: "958530943274",
-    appId: "1:958530943274:web:ac50e79e38ec409e798653",
-    measurementId: "G-LL6B98J5KT"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
